aab98e7a9e8b2e66e068077d80f2ee6e4e7a8e17
et3rnalpain <gjobovi4@mail.ru>
2024-05-06 00:58:45 +0300

    results menu

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/widgets.dart';
 
 class homepage extends StatefulWidget {
   const homepage({super.key});
@@ -28,13 +31,43 @@ class _homepageState extends State<homepage> {
         )
       ),
       body: SafeArea(
-        child: TextButton(
-          onPressed: () {},
-          child: Text("Дима Аверченков"),
+        child:  Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Expanded(
+              child: Row( children: <Widget>[Expanded(
+                  child: LineChart(
+                      LineChartData
+                        (
+                          minX: 0,
+                          minY: 0,
+                          maxX: 50,
+                          maxY: 50,
+                          lineBarsData: [LineChartBarData(
+                              spots: [
+                                FlSpot(0, 0),
+                                FlSpot(10, 15),
+                                FlSpot(20, 25),
+                                FlSpot(30, 15),
+                              ],
+                              isCurved: true
+                          )]
+                      )
+                  )
+              )]
+              ),
+            ),
+          Container(child: Row(children: <Widget>[ Expanded(child: ElevatedButton(
+            onPressed: () {},
+            child: Text("Добавить значение", style: TextStyle(
+              fontFamily: "Josko",
+              fontSize: 20,
+              color: Color.fromARGB(255, 12, 12, 12)
+            ),),
+          ))],),
+          )]
         ),
-
-      ),
-
+    )
     );
   }
 }
