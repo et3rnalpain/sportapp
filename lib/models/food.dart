7e20fe2f613c1f9aa053cf2840c4adcdcf7556b1
et3rnalpain <gjobovi4@mail.ru>
2024-05-05 15:29:13 +0300

    sql

---
+++ lib/models/food.dart
@@ -0,0 +1,8 @@
+class food
+{
+  final int? id;
+  final String name;
+  final int ccal;
+
+  food({this.id,required this.name,required this.ccal});
+}
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -2,19 +2,34 @@ import 'package:flutter/material.dart';
 
 class homepage extends StatefulWidget {
   const homepage({super.key});
-
   @override
   State<homepage> createState() => _homepageState();
 }
 
 class _homepageState extends State<homepage> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color.fromRGBO(228, 228, 148, 16),
-        title: Text("Averchenkov App"),
-      )
+          backgroundColor: Color.fromARGB(255, 151, 251, 87),
+        title: Center(
+          child:
+            Text("Цели",
+                style: TextStyle(
+                fontFamily: "Josko",
+                fontSize: 20,
+                color: Color.fromARGB(255, 12, 12, 12))
+            ),
+        )
+      ),
+      body: SafeArea(
+        child: TextButton(
+          onPressed: () {},
+          child: Text("Дима Аверченков"),
+        ),
+
+      ),
 
     );
   }
