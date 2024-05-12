df20be177d4962b1f3b2c2a90ffa803991c6b965
et3rnalpain <gjobovi4@mail.ru>
2024-05-12 16:39:03 +0300

    moood

--- lib/pages/food.dart
+++ lib/pages/food.dart
@@ -148,7 +148,7 @@ class _foodpageState extends State<foodpage> {
                     ), //Padding
                   ),
                     elevation: 4.0,
-                    child: Column(
+                    /*child: Column(
                       children: [
                         ListTile(
                           title: Text(snapshot.data?.docs[index].get('name')),
@@ -171,7 +171,7 @@ class _foodpageState extends State<foodpage> {
                           children: [
                             IconButton(
                               icon: Icon(Icons.camera),
-                              onPressed: () {getImage(); uploadPic(context);},
+                              onPressed: () {*//*getImage(); uploadPic(context);*//*},
                             ),
                             IconButton(
                               icon: Icon(Icons.food_bank_outlined),
@@ -186,7 +186,7 @@ class _foodpageState extends State<foodpage> {
                           ],
                         )
                       ],
-                    )
+                    )*/
                 );
               });
         },
