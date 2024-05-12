d90b98be8515c8a92a6729ce6c4266bae717d298
stereob1tch <nikita.dolb@yandex.ru>
2024-05-12 22:59:54 +0300

    dime help

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class UserPage extends StatefulWidget {
   const UserPage({super.key});
 
@@ -32,6 +34,23 @@ class _UserPageState extends State<UserPage> {
                   backgroundImage: AssetImage('images/chel.jpg'),
                 ),)),
               ],
+            ),
+            Row(
+              children: [
+                StreamBuilder(
+                  stream: FirebaseFirestore.instance.collection('profile').snapshots(),
+                  builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                    if (!snapshot.hasData) return const Text('Нет данных');
+                    return Row(
+                      children: [
+                        Text(snapshot.data?.docs[0].get('name'),),
+                        Text(snapshot.data?.docs[0].get('weight'),),
+                        Text(snapshot.data?.docs[0].get('height'),)
+                      ],
+                    );
+                  }
+                ),
+              ]
             ),
             Row(
               children: [SizedBox(width: 30,),Text("Твой nickname: ", style: TextStyle(fontFamily: "Josko", fontSize: 20)),
@@ -83,7 +102,13 @@ class _UserPageState extends State<UserPage> {
             Row(
               children: [
                 Expanded(child: TextButton(child: Text("Sperma"),onPressed: (){
-
+                  FirebaseFirestore.instance.collection("profile").doc('0qGL2B0vE094290kGXPl').update(
+                      {
+                        'name': name,
+                        'weight': weight,
+                        'height': height,
+                      }
+                  );
                 },)),
               ],
             )
